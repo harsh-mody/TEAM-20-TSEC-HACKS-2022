26e9968082fdaa0810f56a986f7d10edec635a26
Muskan <muskan80525@gmail.com>
2022-03-09 11:12:30 +0530

    feat:server setup

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const fs = require('fs');
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3001;
+const bcrypt = require("bcrypt");
+
+const connection = require("./utils/dbconnection");
+const publicDirectory = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+app.use(express.static(publicDirectory));
+app.use(express.json());
+
+app.use(flash());
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+
+//allow to access data from req
+app.use(express.urlencoded({ extended: false }));
+
+//ENDPOINTS
+app.get('/', (req, res) => {
+    res.render("home")
+})
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`Server is running at http://${hostname}:${port}/`)
+})
